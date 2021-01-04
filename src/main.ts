44f7f81b5f9d71df123f5bb5c19b9559b09bff18
zicjin <zicjin@gmail.com>
2021-01-04 23:02:17 +0800

    temp

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { app, BrowserWindow } from 'electron'
 import { fork, ChildProcess } from 'child_process'
 import findOpenSocket from './utils/find-open-socket'
 
-if (!app) {
-  throw new Error('unready!')
-}
-
 const isProd = process.env.ELECTRON_ENV === 'production'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -68,7 +64,7 @@ function createBackendProcess(socketName: string) {
   })
 }
 
-app.on('ready', async () => {
+app?.on('ready', async () => {
   console.log('ready!')
   const serverSocket = await findOpenSocket()
 
@@ -81,7 +77,7 @@ app.on('ready', async () => {
   }
 })
 
-app.on('before-quit', () => {
+app?.on('before-quit', () => {
   if (backendProcess) {
     backendProcess.kill()
     backendProcess = null
@@ -91,13 +87,13 @@ app.on('before-quit', () => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
+app?.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-// app.on('activate', () => {
+// app?.on('activate', () => {
 //   // On OS X it's common to re-create a window in the app when the
 //   // dock icon is clicked and there are no other windows open.
 //   if (BrowserWindow.getAllWindows().length === 0) {
